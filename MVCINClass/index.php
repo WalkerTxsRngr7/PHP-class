34bc50c2b2028df722d4a829a4f588223c1c2541
WalkerTxsRngr7 <walkergross7@gmail.com>
2019-04-01 09:05:10 -0500

    fixed locations after move

--- MVCINClass/index.php
+++ MVCINClass/index.php
@@ -2,7 +2,8 @@
     include "./views/header.php";
 
     echo("<a href='./product_catalog'>Catalog</a><br>");
-    echo("<a href='./product_manager'>Manager</a>");
+    echo("<a href='./product_manager'>Manager</a><br>");
+    echo("<a href='./employee'>Employee</a>");
 
     include "./views/footer.php";
 
