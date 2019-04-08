2614b6fbd210ef238fdd0dd87fb7eecef815093d
WalkerTxsRngr7 <walkergross7@gmail.com>
2019-04-08 09:26:36 -0500

    calculates dates and error messages

--- datePractice/index.php
+++ datePractice/index.php
@@ -31,25 +31,51 @@ switch ($action) {
         $due_date_s = filter_input(INPUT_POST, 'due_date');
 
         // make sure the user enters both dates
+        if ($invoice_date_s == NULL || $due_date_s == NULL){
+            $message = 'Enter both dates.';
+        }
 
         // convert date strings to DateTime objects
         // and use a try/catch to make sure the dates are valid
+        $valid = FALSE;
+        try {
+            $invoice_date_o = new DateTime($invoice_date_s);
+            $due_date_o = new DateTime($due_date_s);
+            date_default_timezone_set('America/Chicago');
+            $current_date_o = new DateTime();
+            $valid = TRUE;
+        } catch (Exception $e) {
+            $message = 'Dates are invalid.';
+        }
 
-        // make sure the due date is after the invoice date
+        if ($valid){
+            // make sure the due date is after the invoice date
+            $time_span = $invoice_date_o->diff($due_date_o); 
+            if ($time_span->format('%R') == '-'){
+                $message = 'Due date is before invoice date';
+            }
+            // format both dates
+            $invoice_date_f = $invoice_date_o->format('F d, Y');
+            $due_date_f = $due_date_o->format('F d, Y');
+            
+            // get the current date and time and format it
+            $current_date_f = $current_date_o->format('F d, Y');
+            $current_time_f = $current_date_o->format('g:i:s a');
+            
+            // get the amount of time between the current date and the due date
+            $time_span1 = $current_date_o->diff($due_date_o);
+            ;
+            // and format the due date message
+            if ($time_span1->format('%R') == '-'){
+                $due_date_message = $time_span1->format('This invoice is %y years, %m months, and %d days overdue.');
+            } else {
+                $due_date_message = $time_span1->format('This invoice is due in %y years, %m months, and %d days.');
+            }
+            
 
-        // format both dates
-        $invoice_date_f = 'not implemented yet';
-        $due_date_f = 'not implemented yet'; 
+            break;
+        }
         
-        // get the current date and time and format it
-        $current_date_f = 'not implemented yet';
-        $current_time_f = 'not implemented yet';
-        
-        // get the amount of time between the current date and the due date
-        // and format the due date message
-        $due_date_message = 'not implemented yet';
-
-        break;
 }
 include 'date_tester.php';
 ?>
